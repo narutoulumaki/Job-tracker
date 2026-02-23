e7b2669f2fbbdc688e0f5ac8905ec1e50faafde6
narutoulumaki <bharadhwajdace@gmail.com>
2026-02-23 16:35:35 +0530

    production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,8 +57,19 @@ config.active_job.queue_adapter = :async
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+config.action_mailer.default_url_options = { host: "job-tracker-2-ftkt.onrender.com/" }
 
+config.action_mailer.delivery_method = :smtp
+
+config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: "gmail.com",
+  user_name: ENV["SMTP_USERNAME"],
+  password: ENV["SMTP_PASSWORD"],
+  authentication: "plain",
+  enable_starttls_auto: true
+}
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
   #   user_name: Rails.application.credentials.dig(:smtp, :user_name),
